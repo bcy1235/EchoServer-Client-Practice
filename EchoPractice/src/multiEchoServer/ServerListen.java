1a09f2888d6888bba151eb04c50867521e6440ad
bcy1235 <bcy1235@naver.com>
2024-03-23 19:02:11 +0900

    MultiVersion Complete

---
+++ EchoPractice/src/multiEchoServer/ServerListen.java
@@ -0,0 +1,25 @@
+package multiVersion;
+
+import multiEchoServer.ServerEcho;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListen {
+    private static int count = 0;
+    public static void main(String[] args) {
+        System.out.println("Echo Server on!!!");
+        try {
+            ServerSocket serverSocket = new ServerSocket(54321);
+
+            while (true) {
+                Socket acceptedSocket = serverSocket.accept();
+                System.out.println(++count + "th Thread's connected!!");
+                new Thread(new ServerEcho(acceptedSocket, count)).start();
+            }
+        } catch (IOException ioException) {
+            System.out.println("IOException occurred : " + ioException);
+        }
+    }
+}
